7e0efd453cccbdc5150c43d2819a245d0925f624
Luiz Nogueira <luizastolo@gmail.com>
2021-12-29 18:51:00 -0300

    finished my first project git

---
+++ index.js
@@ -0,0 +1,85 @@
+const express = require ('express')
+const uuid = require('uuid')
+const port = 3000
+
+const app = express()
+app.use(express.json())
+
+/*
+    - Quey params => meusite.com/users?nome=rodolfo&age=28   // FILTROS 
+    - Route params => /users/2   // BUSCAR, DELETAR OU ATUALIZAR ALGO ESPECÍFICO
+
+    - GET          => Buscar informações no bakc-end
+    - POST         => Criar informação no back-end
+    - PUT / PATCH  => Alterar/Atualizar informação no back-end
+    - DELETE       => Deletar informação no bakc-end
+
+    - Middleware   => INTERCEPTADOR => Tem o poder de parar ou alterar dados da requisição
+*/
+
+
+//const { response } = require("express")
+
+const users = []
+
+const checkUserdId = ( request, response, next) => {
+    const { id } = request.params
+
+    const index = users.findIndex( user => user.id === id)
+
+    if (index < 0) {
+        return response.status(404).json({error : " User not found"})
+    }
+
+    request.userIndex = index
+    request.userId = id
+
+    next ()
+}
+
+
+app.get('/users', (request, response) => {
+    return response.json(users)
+})
+
+app.post('/users', (request, response) => {
+    const {name, age} = request.body
+
+    const user = {id:uuid.v4(), name, age}
+
+    users.push(user)
+
+    return response.status(201).json(user)
+})
+
+app.put('/users/:id', checkUserdId, (request, response) => {
+    const { name, age } = request.body
+    const index = request.userIndex
+    const id = request.userId
+
+    const updatedUser = { id, name, age} 
+
+    users[index] = updatedUser
+
+    return response.json(updatedUser)  
+})
+
+app.delete('/users/:id', checkUserdId, (request, response) => {
+    const { id } = request.params
+
+    const index = users.findIndex( user => user.id === id )
+
+    if(index < 0) {
+        return response.status(404).json({message: "User not found"})
+    }
+
+    users.splice(index,1)
+
+    return response.status(204).json()
+
+})
+
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`)
+}) 
